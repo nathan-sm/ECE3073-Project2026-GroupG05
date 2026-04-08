2b7ee51a4bccd5abd030e56baae8c4fd710db4b4
ritwam-s <162244267+ritwam-s@users.noreply.github.com>
2026-04-08 22:05:08 +1000

    ESP-CAM Video stream fix
    Added buffer for webcam feed, improved function calls and fixed esp cam command byte setup.

---
+++ QuartusProj/software/ESP-CAM/obj/default/main.d
@@ -0,0 +1,17 @@
+obj/default/main.o: main.c ../ESP-CAM_bsp/system.h \
+ ../ESP-CAM_bsp/linker.h ../ESP-CAM_bsp//HAL/inc/io.h \
+ ../ESP-CAM_bsp//HAL/inc/alt_types.h \
+ ../ESP-CAM_bsp//drivers/inc/altera_avalon_spi.h \
+ ../ESP-CAM_bsp//HAL/inc/alt_types.h
+
+../ESP-CAM_bsp/system.h:
+
+../ESP-CAM_bsp/linker.h:
+
+../ESP-CAM_bsp//HAL/inc/io.h:
+
+../ESP-CAM_bsp//HAL/inc/alt_types.h:
+
+../ESP-CAM_bsp//drivers/inc/altera_avalon_spi.h:
+
+../ESP-CAM_bsp//HAL/inc/alt_types.h:
