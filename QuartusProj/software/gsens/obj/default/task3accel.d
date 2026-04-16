f8164c45aced68969730650085c64a4d4469a4c4
slui0008 <166362649+jolui0511@users.noreply.github.com>
2026-04-16 14:19:01 +1000

    complete task 3 part 2
    Add connection for accelerometer and output double tap interrupt and axis location

---
+++ QuartusProj/software/gsens/obj/default/task3accel.d
@@ -0,0 +1,25 @@
+obj/default/task3accel.o: task3accel.c ../gsens_bsp/system.h \
+ ../gsens_bsp/linker.h ../gsens_bsp//HAL/inc/io.h \
+ ../gsens_bsp//HAL/inc/alt_types.h \
+ ../gsens_bsp//drivers/inc/altera_avalon_spi.h \
+ ../gsens_bsp//HAL/inc/alt_types.h \
+ ../gsens_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../gsens_bsp//HAL/inc/sys/alt_irq.h ../gsens_bsp//HAL/inc/nios2.h
+
+../gsens_bsp/system.h:
+
+../gsens_bsp/linker.h:
+
+../gsens_bsp//HAL/inc/io.h:
+
+../gsens_bsp//HAL/inc/alt_types.h:
+
+../gsens_bsp//drivers/inc/altera_avalon_spi.h:
+
+../gsens_bsp//HAL/inc/alt_types.h:
+
+../gsens_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../gsens_bsp//HAL/inc/sys/alt_irq.h:
+
+../gsens_bsp//HAL/inc/nios2.h:
