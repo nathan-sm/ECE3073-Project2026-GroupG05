b69723cb0de79ec9be6a95a1253e3e25abc0c187
sowngryphon <zhenyastratford@gmail.com>
2026-04-28 16:20:48 +1000

    Implement quad image display

--- QuartusProj/software/image_system_bsp/system.h
+++ QuartusProj/software/image_system_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'NiosSystem'
  * SOPC Builder design path: ../../NiosSystem.sopcinfo
  *
- * Generated: Thu Apr 16 15:31:46 EST 2026
+ * Generated: Tue Apr 28 16:17:52 EST 2026
  */
 
 /*
@@ -443,7 +443,7 @@
 #define RAM_DUAL_PORT 0
 #define RAM_GUI_RAM_BLOCK_TYPE "AUTO"
 #define RAM_INIT_CONTENTS_FILE "NiosSystem_ram"
-#define RAM_INIT_MEM_CONTENT 1
+#define RAM_INIT_MEM_CONTENT 0
 #define RAM_INSTANCE_ID "NONE"
 #define RAM_IRQ -1
 #define RAM_IRQ_INTERRUPT_CONTROLLER_ID -1
